87815772afde42bae6406ba4b05be35fd7c01366
dmkur <fgameua@gmail.com>
2021-08-19 22:15:50 +0300

    lesson_2 10%

--- lessons/lesson_2/app.js
+++ lessons/lesson_2/app.js
@@ -1,1 +1,19 @@
+//              ліба express -  фреймворк на ноді
 const express = require('express');
+
+//              вказівка app - виеористовуй лібу express
+const app = express();
+
+//              routes
+// get - методи, виділяють наступні методи див нижче
+app.get(`/`, (req, res) => {
+
+});
+
+//
+
+
+//              запуск серверна на порті 5000
+app.listen(5000, ()=>{
+    console.log(`App listen 5000`)
+});
