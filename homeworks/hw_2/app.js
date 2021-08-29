96845154a7b7f822df7e7b21317f190a4ea33b1b
dmkur <fgameua@gmail.com>
2021-08-29 21:27:14 +0300

    hw_2 70%

---
+++ homeworks/hw_2/app.js
@@ -0,0 +1,71 @@
+// Вам потрібно реалізувати мінімум 3 строрінки.
+// 1) Реєстрація
+// 2) Логінація.
+// 3) Список всіх юзерів.
+//
+//     Створити файлик з юзерами, який буде виступати в ролі бази данних.
+//
+//     При реєстрації юзер вводин логін та пороль і ви його данні дописуєте у файлик.
+//     Якщо такий мейл вже є, то видаємо помилку.
+//
+//     При логінації юзер так само ввоить мейл та пароль і вам необхідно знайти юзера в файлі.
+//     Якщо такий мейлик з таким паролем є, то привіти юзера на платформі показати інформацію про нього та кнопочку,
+//     яка перекине нас на список всіх юзерів.
+//     В інакшому випадку сказати, що необхідно реєструватись.
+//
+//     І відображення всіх юзерів це відповідно просто виведення списку вісх юзерів.
+//
+//     При реєстрації мейли не можуть повторюватись
+
+const express = require('express');
+const expressHbs = require('express-handlebars');
+
+const path = require('path');
+const usersDB = require('./db/usersDB');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine', '.hbs');
+app.engine('.hbs', expressHbs({ defaultLayout: false }));
+app.set('views', path.join(__dirname, 'static'));
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.get('/ping', (req, res) => {
+  res.end('Pong');
+});
+
+app.get('/users', (req, res) => {
+  res.render('users', { usersDB });
+});
+
+app.get('/users/:user_id', (req, res) => {
+  const { user_id } = req.params;
+  const singleUser = usersDB[user_id];
+
+  res.render('singleUser', { singleUser });
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', (req, res) => {
+  const { password, email } = req.body;
+  const userSearch = usersDB.find((value) => value.email === email && value.password === password);
+
+  if (userSearch === undefined) {
+    res.redirect('/registration');
+  }
+  res.redirect('/users');
+});
+
+app.get('/registration', (req, res) => {
+  res.render('registration');
+});
+
+app.listen(5000, () => {
+  console.log('App listen 5000');
+});
---
+++ homeworks/hw_2/db/usersDB.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    name: 'Max', age: 23, email: 'max@email.com', password: 'password'
+  },
+  {
+    name: 'Anna', age: 19, email: 'anna@email.com', password: 'password'
+  },
+  {
+    name: 'Oleh', age: 26, email: 'oleh@email.com', password: 'password'
+  },
+  {
+    name: 'Vika', age: 24, email: 'vika@email.com', password: 'password'
+  },
+  {
+    name: 'Alex', age: 56, email: 'alex@email.com', password: 'password'
+  },
+  {
+    name: 'Nastia', age: 215, email: 'nastia@email.com', password: 'password'
+  }
+];
